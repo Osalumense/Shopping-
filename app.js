b06cc490ba13d1edce51359c341292336d2bb029
Osalumense <harkugbeosaz@gmail.com>
2020-12-22 23:56:02 +0100

    Added a feature to update and remove items from the list

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class Item {
 
     create () {
         let listItem = document.createElement('div');
-        listItem.classList.add('list-item');
+        listItem.classList.add('list-item', 'item-actions', 'row', 'd-flex',  'justify-content-center', 'mt-2');
 
         let input = document.createElement('input');
         input.type = 'text';
@@ -18,9 +18,6 @@ class Item {
         input.value = this.name;
         input.disabled = true;
         input.id = 'item-name';
-
-        let actions = document.createElement('div');
-        actions.classList.add('item-actions', 'row', 'd-flex',  'justify-content-center', 'mt-2');
 
         let subaction = document.createElement('div');
         subaction.classList.add('col-sm-8', 'input-group');     
@@ -31,25 +28,36 @@ class Item {
         let updateButton = document.createElement('button');
         updateButton.classList.add('update', 'btn', 'btn-primary');
         updateButton.innerText = 'Update';
+        updateButton.addEventListener('click', () => this.update(input));
 
         let deleteButton = document.createElement('button');
         deleteButton.classList.add('delete', 'btn',  'btn-danger');
         deleteButton.innerText = 'Remove';
+        deleteButton.addEventListener('click', () => this.remove(listItem));
 
         subactionAppend.append(updateButton, deleteButton);
         subaction.append(input, subactionAppend);
-        actions.append(subaction);
-        listItem.append(actions);
+        
+        listItem.append(subaction);
 
         list.append(listItem);
+    }
+
+    update (input) {
+        input.disabled = !input.disabled;
+    }
+
+    remove (listItem) {
+        listItem.parentNode.remove(listItem);
     }
 }
 
 addButton.addEventListener('click', () => newItem());
 
 function newItem() {
-    if (itemToAdd != '') {
+    if (itemToAdd.value !== '') {
         new Item(itemToAdd.value);
+        itemToAdd.value = '';
     }
     else {
         alert('Input field cannot be empty');
